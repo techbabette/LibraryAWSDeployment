b25b74c3a4866bb6bd9345249de8e7fbdb399701
techbabette <marinakrsticrf@gmail.com>
2024-06-03 09:11:38 +0200

    [FEAT] Break main.tf into multiple files

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,54 +1,3 @@
-data "external" "build_vue_app" {
-  program = ["bash", "${path.cwd}/build_vue_app.sh", "https://librarybackend.techbabette.com/api"]
-}
-
-resource "aws_s3_bucket" "vue_app_bucket" {
-}
-
-resource "aws_s3_bucket_public_access_block" "allow" {
-  bucket = aws_s3_bucket.vue_app_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "vue_app_bucket_policy" {
-  bucket = aws_s3_bucket.vue_app_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.vue_app_bucket.arn}/*"
-      }
-    ]
-  })
-
-  depends_on = [ aws_s3_bucket_public_access_block.allow ]
-}
-
-resource "aws_s3_object" "vue_app_files" {
-  for_each = fileset(data.external.build_vue_app.result.path, "**")
-
-  bucket = aws_s3_bucket.vue_app_bucket.bucket
-  key    = each.key
-  source = "${data.external.build_vue_app.result.path}/${each.key}"
-
-    content_type = lookup({
-    "index.html"   = "text/html",
-    "css/app.css" = "text/css",
-    "js/app.js" = "application/javascript",
-    "js/chunk-vendors.js" = "application/javascript",
-  }, each.key, "application/octet-stream")
-
-  depends_on = [ data.external.build_vue_app ]
-}
-
 resource "aws_cloudfront_distribution" "vue_app_distribution" {
   origin {
     domain_name = aws_s3_bucket.vue_app_bucket.bucket_regional_domain_name
@@ -104,8 +53,4 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.vue_app_distribution.domain_name
-}
-
-output "build_output" {
-  value = data.external.build_vue_app.result
 }
---
+++ terraform/s3.tf
@@ -0,0 +1,54 @@
+data "external" "build_vue_app" {
+  program = ["bash", "${path.cwd}/build_vue_app.sh", "https://librarybackend.techbabette.com/api"]
+}
+
+resource "aws_s3_bucket" "vue_app_bucket" {
+}
+
+resource "aws_s3_bucket_public_access_block" "allow" {
+  bucket = aws_s3_bucket.vue_app_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "vue_app_bucket_policy" {
+  bucket = aws_s3_bucket.vue_app_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.vue_app_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [ aws_s3_bucket_public_access_block.allow ]
+}
+
+resource "aws_s3_object" "vue_app_files" {
+  for_each = fileset(data.external.build_vue_app.result.path, "**")
+
+  bucket = aws_s3_bucket.vue_app_bucket.bucket
+  key    = each.key
+  source = "${data.external.build_vue_app.result.path}/${each.key}"
+
+    content_type = lookup({
+    "index.html"   = "text/html",
+    "css/app.css" = "text/css",
+    "js/app.js" = "application/javascript",
+    "js/chunk-vendors.js" = "application/javascript",
+  }, each.key, "application/octet-stream")
+
+  depends_on = [ data.external.build_vue_app ]
+}
+
+output "build_output" {
+  value = data.external.build_vue_app.result
+}
