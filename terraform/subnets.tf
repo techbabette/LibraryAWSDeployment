9c553d51e5732acf2d11b8d2aa19dae6d4159d16
techbabette <marinakrsticrf@gmail.com>
2024-06-04 07:59:39 +0200

    [FEAT] Specify public subnet availability zone

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,6 +1,7 @@
 # SUBNETS
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
+  availability_zone = "eu-north-1a"
   cidr_block = "10.4.1.0/24"
   map_public_ip_on_launch = true
   tags = {
