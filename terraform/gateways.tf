144dd1ded0ea45c43629d905e19bf2bb23d1293e
techbabette <marinakrsticrf@gmail.com>
2024-06-05 18:46:17 +0200

    [FEAT] Add VPC gateway endpoint

--- terraform/gateways.tf
+++ terraform/gateways.tf
@@ -5,3 +5,10 @@ resource "aws_internet_gateway" "igw" {
     Name = "citylibraryigw"
   }
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.eu-north-1.s3"
+
+  route_table_ids = [ aws_route_table.public.id ]
+}
